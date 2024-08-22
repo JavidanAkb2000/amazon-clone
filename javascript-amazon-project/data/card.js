e21e02f9177becdaee00dc064d1eaef0da0b8ac0
JavidanAkb2000 <akberovcavidan1@gmail.com>
2024-08-22 23:34:07 +0200

    Save card in localstorage

--- javascript-amazon-project/data/card.js
+++ javascript-amazon-project/data/card.js
@@ -1,11 +1,22 @@
-export let card = [{
-    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity:2
-},
-{
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:1
-}];
+export let card = JSON.parse(localStorage.getItem('card'));
+
+if(!card){
+    card = [{
+        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity:2
+    },
+    {
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity:1
+    }];
+}
+
+
+
+function saveToStorage(){
+    localStorage.setItem('card',JSON.stringify(card));
+}
+
 
 export function addToCard(productId){
     let matchingItem;
@@ -24,8 +35,9 @@ export function addToCard(productId){
          quantity:1
         });
     }
-    
+    saveToStorage();
 }
+
 
 export function updateCardQuantity(){
     let cardQuantity = 0;
@@ -45,4 +57,5 @@ export function deleteFromCard(productId){
     });
 
     card = newCart;
+    saveToStorage();
 }
