b5164bd1bb92dec3e0972644a0a2daab825347bc
JavidanAkb2000 <akberovcavidan1@gmail.com>
2024-08-28 19:15:31 +0200

    Create class version of the card

---
+++ javascript-amazon-project/data/card-class.js
@@ -0,0 +1,108 @@
+class Card{
+    cartItems;
+    localStorageKey;
+
+    constructor(localStorageKey){
+        
+        this.localStorageKey = localStorageKey;
+        this.loadFromStorage();
+    }
+
+    loadFromStorage(){
+        this.cardItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+        if(!this.cardItems){
+            this.cardItems = [{
+                productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity:2,
+                deliveryOptionId:'1'
+            },
+            {
+                productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                quantity:1,
+                deliveryOptionId:'2'
+            }];
+        }
+    }
+    
+
+    saveToStorage(){
+        localStorage.setItem(this.localStorageKey,JSON.stringify(this.cardItems));
+    }
+
+    addToCard(productId){
+        let matchingItem;
+    
+        this.cardItems.forEach((cardItem)=>{
+         if(productId===cardItem.productId){
+             matchingItem=cardItem;
+         }
+        });
+    
+        if(matchingItem){
+         matchingItem.quantity += 1;
+        }else{
+         this.cardItems.push({
+             productId,
+             quantity:1,
+             deliveryOptionId:'1'
+            });
+        }
+        this.saveToStorage();
+    }
+
+    deleteFromCard(productId){
+        const newCart = [];
+        this.cardItems.forEach((item)=>{
+            if(item.productId !== productId){
+                newCart.push(item);
+            }
+        });
+    
+        this.cardItems = newCart;
+        this.saveToStorage();
+    }
+
+    updateCardQuantity(){
+        let cardQuantity = 0;
+    
+        card.cardItems.forEach((cardItem)=>{
+         cardQuantity+=cardItem.quantity;
+        })
+    
+        document.querySelector('.js-card-quantity').innerHTML=cardQuantity;
+    }
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+        let matchingItem;
+      
+        card.cardItems.forEach((cardItem) => {
+          if (productId === cardItem.productId) {
+            matchingItem = cardItem;
+          }
+        });
+      
+        // Add a check to see if matchingItem was found
+        if (matchingItem) {
+          matchingItem.deliveryOptionId = deliveryOptionId;
+          this.saveToStorage();
+        } else {
+          console.error(`Product with ID ${productId} not found in the cart.`);
+        }
+      }
+
+}
+
+
+
+
+const card = new Card('card-oop');
+const businessCart = new Card('business-oop');
+
+
+console.log(card);
+console.log(businessCart);
+
+console.log(businessCart instanceof Function);
+
+
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/card-oop.js';
+import '../data/card-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
